02f9a21e1484ebd787470ad747f4efd9f207418a
GOURIDO <gouridec2000@gmail.com>
2025-04-15 05:08:05 +0000

    Added JavaScript for interactivity and updated CSS

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript code to change background color when button is clicked
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = "#f0e68c"; // Change to yellow color
+}
